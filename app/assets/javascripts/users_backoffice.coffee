03c561d8ca1ef1974e77d5da821544124f52d1c3
Matheus Martins <matheus.martins@rarolabs.com.br>
2021-11-10 23:07:58 -0300

    .

--- app/assets/javascripts/users_backoffice.coffee
+++ app/assets/javascripts/users_backoffice.coffee
@@ -1,3 +1,9 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+//= require jquery-2.2.4/dist/jquery.min.js
+//= require bootstrap/dist/js/bootstrap.js
+//= require fastclick/lib/fastclick.js
+//= require nprogress/nprogress.js
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js
+//= require custom.min.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,19 +1,33 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 
 # These files are in /app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( 
+   admins_backoffice.js 
+   admins_backoffice.css 
+   users_backoffice.js
+   users_backoffice.css
+)
 
 # These files are in /lib/assets
-Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css )
+Rails.application.config.assets.precompile += %w(
+   sb-admin-2.min.js
+   sb-admin-2.min.css
+   custom.min.js
+   custom.min.css
+)
+
+# These files are in /vendor/assets
+Rails.application.config.assets.precompile += %w(jquery-2.2.4/dist/jquery.min.js)
+
