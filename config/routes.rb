1e0746542406d6774d87d0e829bf9f102ccd64d4
martinsmf <martinsmf00@gmail.com>
2021-11-18 00:15:07 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
   namespace :site do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins, only: [:index, :edit, :update] # Administrators
   end
   namespace :users_backoffice do
     get 'welcome/index'
