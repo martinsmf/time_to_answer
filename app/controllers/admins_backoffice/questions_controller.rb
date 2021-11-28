53b853bd92c65080a69926cca14adfb8fc918a4e
martinsmf <martinsmf00@gmail.com>
2021-11-27 23:14:33 -0300

    .

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,8 +1,11 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:edit, :update, :new, :create]
 
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.includes(:subject)
+      .order(:description)
+      .page(params[:page])
   end
 
   def new
@@ -39,8 +42,12 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   private
 
+  def get_subjects
+    @subjects = Subject.all
+  end
+
   def params_question
-    params.require(:admin).permit(:description, :subject_id)
+    params.require(:question).permit(:description, :subject_id)
   end
 
   def set_question
