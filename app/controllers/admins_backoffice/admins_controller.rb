d7199549831206b3f0c0f447822fa35439436086
martinsmf <martinsmf00@gmail.com>
2022-01-03 22:46:31 -0300

    Knowing Action mailer

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -26,6 +26,7 @@ module AdminsBackoffice
 
     def update
       if @admin.update(params_admin)
+        AdminMailer.update_email(current_admin, @admin).deliver_now
         redirect_to admins_backoffice_admins_path, notice: 'Administrador atualizado com sucesso!'
       else
         render :edit
